04f3e8650d203314e7ae71e128119309067fa7c7
M.ABDUL REHMAN <a03480748044@gmail.com>
2026-09-09 19:24:13 +0500

    day2_agent

---
+++ day2_agent.py
@@ -0,0 +1,49 @@
+import json
+
+
+def calculator(a, b, operation):
+    if operation == "add":
+        return a + b
+
+    elif operation == "subtract":
+        return a - b
+
+    elif operation == "multiply":
+        return a * b
+
+    elif operation == "divide":
+        if b == 0:
+            return "Cannot divide by zero."
+
+        return a / b
+    return "Unknown operation"
+
+
+def agent(request):
+    print("User Request:", request)
+
+    # Agent's decision
+    tool_request = {
+        "tool": "calculator",
+        "operation": "add",
+        "a": 20,
+        "b": 30
+    }
+
+    print("\nAgent Decision:")
+
+    print(json.dumps(tool_request, indent=4))
+
+    # Execute selected tool
+    result = calculator(
+        tool_request["a"],
+        tool_request["b"],
+        tool_request["operation"]
+    )
+
+    return result
+
+
+answer = agent("Please add 20 and 30")
+
+print("\nFinal Answer:", answer)
