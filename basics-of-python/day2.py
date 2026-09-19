3abbb3985ed7a5dd1e166f0522e39c3dee806e25
AbdulRehman4t7 <a03480748044@gmail.com>
2026-09-19 12:12:41 +0500

    Create day2.py
    day 2 py

---
+++ basics-of-python/day2.py
@@ -0,0 +1,26 @@
+# Get user input
+#name = input("Enter your name: ")
+#print("Hello", name)
+
+
+#age = int(input("Enter your age: "))
+#print(age)
+#print(type(age))
+
+#price = float(input("Enter price: "))
+#print(price)
+#print(type(price))
+
+#Simple Bill Calculator
+item = input("Enter item name: ")
+price = float(input("Enter price: "))
+quantity = int(input("Enter quantity: "))
+
+total = price * quantity
+
+print("Item:", item)
+print("Price:", price)
+print("Quantity:", quantity)
+print("Total:", total)
+
+print(10%3)
