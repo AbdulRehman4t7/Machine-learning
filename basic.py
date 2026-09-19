dc553d6503a19a8878d928cc8c034a08cc300742
AbdulRehman4t7 <a03480748044@gmail.com>
2026-09-19 11:54:05 +0500

    python basics

---
+++ basic.py
@@ -0,0 +1,15 @@
+name="Abdul Rahman"
+age= 22
+semester= 7
+print("My name is", name)
+print("My age is", age)
+print("I am in semester", semester)
+
+age=23
+print("My age is", age)
+
+
+price= 100
+quantity= 5
+total= price * quantity
+print("The total cost is", total)
---
+++ basics-of-python/day1.py
@@ -0,0 +1,9 @@
+name="Abdul Rehman"
+age= 22
+semester= 7
+is_student= True
+
+print(type(name))
+print(type(age))
+print(type(semester))
+print(type(is_student))
